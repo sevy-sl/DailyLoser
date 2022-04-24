6ac5dd52c8a6d18f58b6cf2c4edfaa1b309c3021
sevy-sl <gav.stas.yes@gmail.com>
2022-04-24 15:11:56 +0300

    Added inline mode

--- main.py
+++ main.py
@@ -1,62 +1,45 @@
 import logging
-from uuid import uuid4
-from telegram import InlineQueryResultArticle, ParseMode, InputTextMessageContent, Update
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler, CallbackContext
-from telegram.utils.helpers import escape_markdown
+import telegram
+import datetime
+from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
+from telegram.ext import Updater, CommandHandler, CallbackContext, InlineQueryHandler
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 
+bot = telegram.Bot(token='')
 logger = logging.getLogger(__name__)
 
 
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Hi!')
-
+    update.message.reply_text('Pff loser \U0001F612')
 
 def help_command(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Help!')
+    update.message.reply_text('Use me in group chats please :-)')
 
 
 def loser(update: Update, context: CallbackContext) -> None:
-    query = update.inline_query.query
-
-    if query == "":
-        return
-
-    results = [
+    date = datetime.datetime.now().strftime("%d")
+    query_user_name = update.inline_query.from_user.name
+    result = [
         InlineQueryResultArticle(
-            id=str(uuid4()),
-            title="Caps",
-            input_message_content=InputTextMessageContent(query.upper()),
-        ),
-        InlineQueryResultArticle(
-            id=str(uuid4()),
-            title="Bold",
-            input_message_content=InputTextMessageContent(
-                f"*{escape_markdown(query)}*", parse_mode=ParseMode.MARKDOWN
-            ),
-        ),
-        InlineQueryResultArticle(
-            id=str(uuid4()),
-            title="Italic",
-            input_message_content=InputTextMessageContent(
-                f"_{escape_markdown(query)}_", parse_mode=ParseMode.MARKDOWN
-            ),
+            id='loser',
+            title='Today\'s loser?',
+            input_message_content=InputTextMessageContent('Hello! Today\'s loser is %s :-)' % query_user_name)
         ),
     ]
+    update.inline_query.answer(result)
 
-    update.inline_query.answer(results)
 
 
 def main() -> None:
-    upd = Updater("5327300331:AAHDK9ZOPwZGqxgidlGCNseBPIKau25fGNY")
+    upd = Updater('')
     dp = upd.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help_command))
-    dp.add_handler(InlineQueryHandler(inlinequery))
+    dp.add_handler(InlineQueryHandler(loser))
 
     upd.start_polling()
     upd.idle()
